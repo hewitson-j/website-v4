6efca717e59aba2c11ef1a8424d1fb2f2af05965
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-10-10 21:59:12 -0600

    Added Home text components.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="home-main">
+        <div className="home-text">
+          <h1 id="home-intro-text">I'm Jacob, nice to meet you.</h1>
+          <h2 className="home-description" id="home-text-tagline">
+            Looking for a <br />
+            <span id="home-emphasis-text">Frontend Engineer</span>?
+          </h2>
+          <h2 className="home-description">
+            <br />
+            I'm passionate about the apps I build.
+            <br />
+            Let's see what I can do for you.
+          </h2>
+          <br />
+          <button>
+            <Link to={"projects"} className="home-link">
+              See Projects
+            </Link>
+          </button>
+        </div>
+        <div className="home-pic"></div>
+      </div>
     </>
   );
 }
