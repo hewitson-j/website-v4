201df30aa0f5a1a7c86b9db637babd7ad2e7fc53
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-02-01 10:14:50 -0700

    Added other services route.

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
+import OtherServices from "./OtherServices";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="projects" element={<Projects />} />
+      <Route path="other-services" element={<OtherServices />} />
       <Route path="contact" element={<Contact />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
