fd9e8207ea5ff671c538294a1d64ecf587511ac2
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-05-04 12:33:12 -0600

    Added about sections and styling for intro

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,17 @@
 import './About.css'
+import profilePic from '../images/Capitol Profile Pic.jpg'
+
 
 export default function About(){
     return <div className='about'>
-        <h1>About Me</h1>
+        <h1>Nice to Meet You!</h1>
+        <div className='about-intro'>
+            <p>It's really nice to meet you. My name is Jacob Hewitson and I am a software engineer working with <a href='http://www.familysearch.org'>FamilySearch</a>. Here you can get to know a little more about me!</p>
+            <img src={profilePic} id='about-profile-pic' alt='My Profile Pic' />
+        </div>
+        <h2>School</h2>
+            <p></p>
+        <h2>Work</h2>
+
     </div>
 }
