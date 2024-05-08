0bc865467f424e9ef93ba9a8e1b1d93d3f09e34d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-05-08 00:27:05 -0600

    Added styling for about and copyright

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,18 +10,18 @@ export default function About(){
     return <div className='about'>
         <h1>Nice to Meet You!</h1>
         <div className='about-intro'>
-            <p>It's really nice to meet you. My name is Jacob Hewitson and I am a software engineer working with <a href='http://www.familysearch.org'>FamilySearch</a>. Here you can get to know a little more about me!</p>
             <img src={profilePic} id='about-profile-pic' alt='My Profile Pic' />
+            <p>It's really nice to meet you. My name is Jacob Hewitson and I am a software engineer working with <a href='http://www.familysearch.org'>FamilySearch</a>. Here you can get to know a little more about me!</p>
         </div>
         <h2>School</h2>
             <p className='paragraphs'>
                 I graduated with my Bachelor of Computer Science from Western Governors Univerisity in November, 2022. Below are my credentials and certifications:
-                <ul>
+            </p>
+            <ul className='paragraphs'>
                     <li>BS of Science, Computer Science from Western Governors University (Graduated Nov. 2022)</li>
                     <li>CompTIA Project+ Certification (issued Jun. 2022)</li>
                     <li>ITIL v4 Foundations Certificate (issued Jun. 2022)</li>
-                </ul>
-            </p>
+            </ul>
         <h2>Work</h2>
         <p className='paragraphs'>Currently I am working full-time as a web development intern for <a href='http://www.familysearch.org'>FamilySearch</a>. I also manage web applications and websites for several organizations and clients such as <a href='http://leapsandboundsprek.com'>Leaps and Bounds Preschool, Colorado Springs</a> and the <a href='http://minegrinder.com'>Mine Grinder</a> book series</p>
         <h2>Reach out!</h2>
