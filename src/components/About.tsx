4ba931b2f8a721effa12164b2856ceb37ed60c4e
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-05-11 12:17:33 -0600

    Adjusted margin stylings, wrapped sections around paragraphs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,8 @@ export default function About(){
             <img src={profilePic} id='about-profile-pic' alt='My Profile Pic' />
             <p>It's really nice to meet you. My name is Jacob Hewitson and I am a software engineer working with <a href='http://www.familysearch.org'>FamilySearch</a>. Here you can get to know a little more about me!</p>
         </div>
-        <h2>School</h2>
+        <section>
+            <h2>School</h2>
             <p className='paragraphs'>
                 I graduated with my Bachelor of Computer Science from Western Governors Univerisity in November, 2022. Below are my credentials and certifications:
             </p>
@@ -22,12 +23,17 @@ export default function About(){
                     <li>CompTIA Project+ Certification (issued Jun. 2022)</li>
                     <li>ITIL v4 Foundations Certificate (issued Jun. 2022)</li>
             </ul>
-        <h2>Work</h2>
-        <p className='paragraphs'>Currently I am working full-time as a web development intern for <a href='http://www.familysearch.org'>FamilySearch</a>. I also manage web applications and websites for several organizations and clients such as <a href='http://leapsandboundsprek.com'>Leaps and Bounds Preschool, Colorado Springs</a> and the <a href='http://minegrinder.com'>Mine Grinder</a> book series</p>
-        <h2>Reach out!</h2>
-        <p className='paragraphs'>I'm always looking to share what I'm working on. Feel free to reach out by clicking this link <Link to={'/contact'}>here</Link> or by clicking the button below.</p>
-        <button onClick={() => navigate('/contact')}
-        className='buttons'>Contact Me!</button>
+        </section>
+        <section>
+            <h2>Work</h2>
+            <p className='paragraphs'>Currently I am working full-time as a web development intern for <a href='http://www.familysearch.org'>FamilySearch</a>. I also manage web applications and websites for several organizations and clients such as <a href='http://leapsandboundsprek.com'>Leaps and Bounds Preschool, Colorado Springs</a> and the <a href='http://minegrinder.com'>Mine Grinder</a> book series</p>
+        </section>
+        <section>
+            <h2>Reach out!</h2>
+            <p className='paragraphs'>I'm always looking to share what I'm working on. Feel free to reach out by clicking this link <Link to={'/contact'}>here</Link> or by clicking the button below.</p>
+            <button onClick={() => navigate('/contact')}
+            className='buttons'>Contact Me!</button>
+        </section>
         <Copyright margin='1rem auto'/>
     </div>
 }
