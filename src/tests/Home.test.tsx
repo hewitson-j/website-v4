26ac271cee7625d06656affefd464ebd2d06327d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-07-22 23:37:42 -0600

    Added tests

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,12 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../components/Home";
+import { MemoryRouter } from "react-router-dom";
 
 test ('first test', () => {
     expect(true).toBe(true)
 })
 
-test ('renders intro text', () => {
-    render(<Home/>)
+test('renders intro text', () => {
+    render(<MemoryRouter>
+        <Home/>
+    </MemoryRouter>)
     const introText = screen.getByText(/I'm Jacob, nice to meet you./i)
     expect(introText).toBeInTheDocument()
+})
+
+test('renders image', () => {
+    render(<MemoryRouter>
+        <Home/>
+    </MemoryRouter>)
+
+    const image = screen.getByAltText(/Jacob's Profile Picture/i)
+
+    expect(image).toBeInTheDocument()
 })
