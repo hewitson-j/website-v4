1eba9f651993372669ea69a29709e0d776366baf
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-09-28 01:16:41 -0600

    Added otherservices test

---
+++ src/tests/OtherServices.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import OtherServices from "../components/OtherServices";
+import { MemoryRouter } from "react-router-dom";
+
+const MockOtherServicesComponent = () => {
+  return (
+    <MemoryRouter>
+      <OtherServices />
+    </MemoryRouter>
+  );
+};
+
+test("renders OtherServices", () => {
+  render(<MockOtherServicesComponent />);
+  const notaryText = screen.getByText(/Notary Public - Utah, Remote/i);
+  expect(notaryText).toBeInTheDocument();
+});
